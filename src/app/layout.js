4834d1d5ebd005206ce1920efbb7952b9d6e49d4
David West <davidpwest1220@gmail.com>
2025-08-07 14:08:43 -0500

    Installs done - next / tailwind files set up.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// WHERE TO ADD NAVBAR & FOOTER
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
